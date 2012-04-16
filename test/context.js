775078c940f65d6e2c3cbe25a292caa77cba5d92
Administrator <sailor@ubuntu.ubuntu-domain>
2012-04-16 17:40:48 +0800

    misc:add config and master console

--- test/context.js
+++ test/context.js
@@ -23,6 +23,19 @@ User.prototype.__defineGetter__('xcount',function(){return this.count;});
 
 var user = new User(4);
 
+var os=require('os');
+var ifaces=os.networkInterfaces();
+for (var dev in ifaces) {
+  var alias=0;
+  ifaces[dev].forEach(function(details){
+    if (details.family=='IPv4') {
+      console.log(dev+(alias?':'+alias:''),details.address);
+      ++alias;
+    }
+  });
+}
+
+
 console.log(user.xcount);
 
 console.log(util.inspect(user,true,100,true));
